428849aa4e1dd08a3df6fbb39da032a8c8dbcc73
Eliška Korencová <eli.korenc@gmail.com>
2023-04-10 21:00:31 +0200

    Add the move fun - change current-Player icon o/x & location o/x into  squares.

--- hra.js
+++ hra.js
@@ -9,37 +9,32 @@ const currentPlayerElm = document.querySelector('.current-player');
 
 //
 
-const circleLocation = (event) => {
-  event.target.classList.add('square-circle');
+const move = (event) => {
+  // fun condition for choosing to move o/x markers into a square:
+  if (currentPlayer === circle) {
+    event.target.classList.add('square-circle');
+  } else {
+    event.target.classList.add('square-cross');
+  }
+
+  // fun condition to change the current player icon:
+  if (currentPlayer === circle) {
+    currentPlayer = cross;
+  } else {
+    currentPlayer = circle;
+  }
+  currentPlayerElm.classList.toggle('player-cross');
+  currentPlayerElm.classList.toggle('player-circle');
 };
 
-document
-  .querySelector('button:nth-child(1)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(2)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(3)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(4)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(5)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(6)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(7)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(8)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(9)')
-  .addEventListener('click', circleLocation);
-document
-  .querySelector('button:nth-child(10)')
-  .addEventListener('click', circleLocation);
+// event listener:
+document.querySelector('button:nth-child(1)').addEventListener('click', move);
+document.querySelector('button:nth-child(2)').addEventListener('click', move);
+document.querySelector('button:nth-child(3)').addEventListener('click', move);
+document.querySelector('button:nth-child(4)').addEventListener('click', move);
+document.querySelector('button:nth-child(5)').addEventListener('click', move);
+document.querySelector('button:nth-child(6)').addEventListener('click', move);
+document.querySelector('button:nth-child(7)').addEventListener('click', move);
+document.querySelector('button:nth-child(8)').addEventListener('click', move);
+document.querySelector('button:nth-child(9)').addEventListener('click', move);
+document.querySelector('button:nth-child(10)').addEventListener('click', move);
